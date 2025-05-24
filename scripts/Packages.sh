a46fbfa76d89a1b94c4d911ef8c266c5fc77ccaf
oussamabenalisst <benalio248@gmail.com>
2025-05-24 17:15:35 +0200

    update dh script add echo

--- scripts/Packages.sh
+++ scripts/Packages.sh
@@ -1,4 +1,6 @@
+echo "Updating package list..."
 sudo apt update 
+echo "Installing Python3 and required packages..."
 sudo apt install python3
 sudo pip install pandas
 sudo pip install pyfiglet
@@ -6,3 +8,4 @@ sudo pip install colorama rich
 sudo pip install random
 sudo pip install time
 sudo pip install os
+echo "Python3 and packages installed successfully."
